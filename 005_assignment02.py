cac3f4b36556c2a3a2a3dbc9ef1a40d50f85c30b
jshnprt <95943390+jshnprt@users.noreply.github.com>
2021-12-12 15:05:22 -0500

    Add files via upload
    assignment submitted

---
+++ 005_assignment02.py
@@ -0,0 +1,11 @@
+x=input("Input the side a length: ")
+a_side=float(x)
+y=input("Input the side b length: ")
+b_side=float(y)
+c=((a_side**2+b_side**2)**0.5)
+print("The Hypotenuse length is: ",c)
+
+
+#our_name=input("Enter your name:")
+#print("Hello", your_name)
+
