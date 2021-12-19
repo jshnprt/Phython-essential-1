3f467916c36c058a3d134dbea5756cade1947988
jshnprt <95943390+jshnprt@users.noreply.github.com>
2021-12-19 14:17:17 -0500

    Add files via upload
    assignment

--- assignment018_for.py
+++ assignment018_for.py
@@ -1,7 +1,8 @@
 n=int(input("enter a number: "))
 print("the divisor of the given number are:")
-for i in range (1,n+1):
-    if (n%i==0):
-        print(i)
+for i in range (1,n//2+1):
+    if (n%i)==0:
+        print(i,end=", ")
+print(n)
 
     
